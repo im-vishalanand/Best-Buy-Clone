2613313b1df8718fcb01197f296448c348d65f53
Kumar Vishal Anand <iam.vishalanand11@gmail.com>
2022-11-13 11:27:30 +0530

    day-5

--- cart/cart.js
+++ cart/cart.js
@@ -11,10 +11,20 @@ console.log(LSData)
 let container=document.querySelector('#left-cont');
 let container2=document.querySelector('#right-cont');
 
+let cartPrice=LSData.reduce((acc,ele)=>{
+    return acc+(+ele.price);
+  },0);
+
+
+//   let LSPrice=localStorage.getItem('buy')||0
+  let LSPrice=cartPrice
+
+  console.log(cartPrice)
+
 function displayData(data){
     container.innerHTML=null
 
-    data.forEach((element) => {
+    data.forEach((element, index) => {
 
         let div=document.createElement('div');
 
@@ -34,12 +44,15 @@ function displayData(data){
         subButton.innerText="-"
         subButton.addEventListener('click', function(){
             qty.innerText=+(qty.innerText)-1;
-            totalPrice-=(+element.price)
-            document.querySelector('#cart-total').innerText=totalPrice
-            if(qty.innerText==0){
+            cartPrice-=(+element.price)
+            document.querySelector('#priceTotal').innerText=cartPrice;
+            totalfig.innerText=cartPrice-585;
+            localStorage.setItem('buy', cartPrice);
+
+            if(qty.innerText=="0"){
                 data.splice(index,1);
-                localStorage.setItem('cart',JSON.stringify(data))
-                displayData(LSData)
+                localStorage.setItem('cartData',JSON.stringify(data))
+                displayData(data);
             }
         })
         let qty=document.createElement('span');
@@ -48,15 +61,17 @@ function displayData(data){
         addButton.innerText="+"
         addButton.addEventListener('click', function(){
           qty.innerText=+(qty.innerText)+1;
-          totalPrice+=(+element.price)
-          document.querySelector('#cart-total').innerText=totalPrice
+          cartPrice+=(element.price)*(+qty.innerText-1)
+          document.querySelector('#priceTotal').innerText=cartPrice;
+          totalfig.innerText=cartPrice-585;
+          localStorage.setItem('buy', cartPrice);
         })
         let remove=document.createElement('button');
         remove.innerText="Remove";
         remove.addEventListener('click', function(){
             data.splice(index,1);
-            localStorage.setItem('cart',JSON.stringify(data))
-            displayData(LSData);
+            localStorage.setItem('cartData',JSON.stringify(data))
+            displayData(data);
             window.location.reload()
         })
         nameDiv.append(name,price,subButton,qty,addButton,remove)
@@ -66,27 +81,56 @@ function displayData(data){
         
         
     });
+    let rightDiv=document.createElement('div');
+    container2.append(rightDiv)
+    let h2=document.createElement('h2');
+    h2.innerText='Price details';
+    let price=document.createElement('div')
+    let priceName=document.createElement('p')
+    priceName.innerText="Price"
+    let pricefig=document.createElement('p');
+    pricefig.setAttribute('id','priceTotal')
+    pricefig.innerText=cartPrice;
+    price.append(priceName,pricefig)
+    
+    let discount=document.createElement('div')
+    let discountName=document.createElement('p')
+    discountName.innerText="Discount"
+    let discountfig=document.createElement('p');
+    discountfig.innerText="- 495"
+    discount.append(discountName,discountfig)
+    
+    let delivery=document.createElement('div')
+    let deliveryName=document.createElement('p')
+    deliveryName.innerText="Delivery"
+    let deliveryfig=document.createElement('p');
+    deliveryfig.innerText="- 90"
+    delivery.append(deliveryName,deliveryfig)
+    
+    let total=document.createElement('div')
+    let totalName=document.createElement('p')
+    totalName.innerText="Total Amount"
+    let totalfig=document.createElement('p');
+    totalfig.innerText=cartPrice-585;
+    total.append(totalName,totalfig)
+    
+    let placeOrder=document.createElement('button');
+    
+    placeOrder.innerText='Place Order'
+    placeOrder.setAttribute('class', 'placeOrder')
+    
+    
+    rightDiv.append(h2,price,discount,delivery, total,placeOrder);
+    
+    
+    document.querySelector('.placeOrder').addEventListener('click', function(){
+        console.log('ghjk')
+    })
+    document.querySelector('.placeOrder').addEventListener('click', function(){
+        window.location.href='/bustling-wheel-1520/payment/payment.html'
+    })
 }
-let rightDiv=document.createElement('div');
-container2.append(rightDiv)
-let h2=document.createElement('h2');
-h2.innerText='Price details';
-let price=document.createElement('div')
-let priceName=document.createElement('p')
-priceName.innerText="Price"
-price.append(priceName)
-let pricefig=document.createElement('p');
-pricefig.append(fig)
-let discount=document.createElement('div')
-let discountName=document.createElement('p')
-discountName.innerText="Price"
-price.append(priceName)
-let discountfig=document.createElement('p');
-price.append(fig)
-
-
-
-rightDiv.append(h2,price);
 
 
 displayData(LSData)
+
